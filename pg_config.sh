83693243506a630b8cfc6e57db10eb41d6b9f812
vagrant <vagrant@vagrant.vm>
2018-09-29 10:49:39 +0000

    update after lesson 2

--- pg_config.sh
+++ pg_config.sh
@@ -4,9 +4,9 @@ apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-sqlalchemy
 apt-get -qqy install python-pip
 pip install --upgrade pip
-pip install werkzeug==0.8.3
-pip install flask==0.9
-pip install Flask-Login==0.1.3
-pip install oauth2client
-pip install requests
-pip install httplib2
+pip2 install werkzeug==0.8.3
+pip2 install flask==0.9
+pip2 install Flask-Login==0.1.3
+pip2 install oauth2client
+pip2 install requests
+pip2 install httplib2
